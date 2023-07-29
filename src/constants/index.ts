729c0062b3b62ad132917cb7197d098f49555e63
chintrinh-groove <chin.trinh@groovetechnology.com>
2023-07-29 12:13:47 +0700

    Feature: fix node env

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export const env = process.env
-export const IS_DEVELOP = true || process.env.NODE_ENV === 'develop'
+export const IS_DEVELOP = true && process.env.NODE_ENV === 'develop'
 export const IS_SAMPLE_QUERY = !true
